b35f8f75546acb72c8c7ca2f1356e6978a274649
edi-marc <edi-marc@student.42roma.it>
2021-02-14 13:35:03 +0100

    ft_memcpy changed

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,24 +6,24 @@
 /*   By: edi-marc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 10:47:45 by edi-marc          #+#    #+#             */
-/*   Updated: 2021/01/14 12:59:11 by edi-marc         ###   ########.fr       */
+/*   Updated: 2021/02/14 13:34:04 by edi-marc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-    size_t i;
+	size_t	i;
 
-    i = 0;
-    if (src && dst)
-    {
-        while (i < n)
-        {
-            ((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
-            i++;
-        }
-    }
-    return (dst);
+	i = 0;
+	if (src && dst)
+	{
+		while (i < n)
+		{
+			((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+			i++;
+		}
+	}
+	return (dst);
 }
