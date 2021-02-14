8e508b06d51322881e15c91bc3b350fe627d28fe
Null-v <75049426+Null-v@users.noreply.github.com>
2021-02-14 13:31:08 +0100

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -12,17 +12,18 @@
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void    *ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t i;
+    size_t i;
 
-	i = 0;
-	if (!src && !dst)
-		return (dst);
-	while (i < n)
-	{
-		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
-		i++;
-	}
-	return (dst);
+    i = 0;
+    if (src && dst)
+    {
+        while (i < n)
+        {
+            ((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+            i++;
+        }
+    }
+    return (dst);
 }
